4863f879fdb7a724225e38efae7c33b70014ae3f
Sukalpo Mitra <sukalpomitra@gmail.com>
2019-07-03 14:08:36 +0800

    web hook for getting verification key

---
+++ aries_cloudagent/wallet/routes.py
@@ -0,0 +1,57 @@
+"""Connection handling admin routes."""
+
+from aiohttp import web
+from aiohttp_apispec import docs, request_schema, response_schema
+
+from marshmallow import fields, Schema
+
+from .models.wallet_verification_key import WalletVerificationKeySchema
+
+from .manager import WalletManager
+
+
+class VerificationKeySchema(Schema):
+    """Result schema for getting verification key."""
+
+    results = fields.List(fields.Nested(WalletVerificationKeySchema()))
+
+
+@docs(
+    tags=["wallet"],
+    summary="Cloud agent verification key",
+    parameters=[],
+)
+
+
+@docs(tags=["wallet"], summary="Get verification key")
+@response_schema(VerificationKeySchema(), 200)
+async def get_verification_key(request: web.BaseRequest):
+    """
+    Request handler for getting cloud agent verification key.
+
+    Args:
+        request: aiohttp request object
+
+    Returns:
+        The verification key
+
+    """
+    context = request.app["request_context"]
+
+    seed = request.match_info["id"]
+    wallet_mgr = WalletManager(context)
+    walletverificationkey = await wallet_mgr.get_verification_key(seed)
+    result = {
+        "key": walletverificationkey.key,
+    }
+    return web.json_response(result)
+
+
+async def register(app: web.Application):
+    """Register routes."""
+
+    app.add_routes(
+        [
+            web.get("/wallet/{id}", get_verification_key),
+        ]
+    )
